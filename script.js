c1d53a0055d93ca5a351f7c7930479414902814d
Serena Roderick <78286589+serenaroderick@users.noreply.github.com>
2023-03-27 22:48:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mouseOver(x) {
+    x.style.color = "white";
+    x.style.textDecoration = "underline";
+    x.style.textUnderlinePosition = "under";
+
+}
+function mouseOut(x) {
+    x.style.color = "#a9a9a9";
+    x.style.textDecoration = "none";
+}
